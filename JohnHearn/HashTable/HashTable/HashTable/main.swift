9412813bfccac39d8fbbf749c5a5af6caa084512
John Hearn <jhearn@gmail.com>
2016-12-05 18:04:44 -0800

    Implemented a hash table with some basic load balancing

---
+++ JohnHearn/HashTable/HashTable/HashTable/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  HashTable
+//
+//  Created by John D Hearn on 12/5/16.
+//  Copyright © 2016 Bastardized Productions. All rights reserved.
+//
+
+import Foundation
+
+
+print("Hello, World!")
+
