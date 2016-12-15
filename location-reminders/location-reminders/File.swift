bc641f2e139b098b30c0296ff629c0dedcb88baf
Corey Malek <coreymalek@coreys-macbook-pro.local>
2016-12-15 08:56:34 -0800

    created bridging header

---
+++ location-reminders/location-reminders/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  location-reminders
+//
+//  Created by Corey Malek on 12/15/16.
+//  Copyright © 2016 Corey Malek. All rights reserved.
+//
+
+import Foundation
---
+++ location-reminders/location-reminders/location-reminders-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "LogInViewController.h"
+#import "ViewController.h"
+#import "Reminder.h"
+#import "LocationController.h"
